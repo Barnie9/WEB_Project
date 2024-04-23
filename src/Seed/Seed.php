da6e4f831f5dcf2e4e9ce737e30a891ec9fb3cb8
Barnie <bran.alexandru.2002@gmail.com>
2024-04-23 21:33:41 +0300

    tweaked table name

--- src/Seed/Seed.php
+++ src/Seed/Seed.php
@@ -2,7 +2,7 @@
 
 $db = $container->get('db');
 
-$createUsersTable = "CREATE TABLE IF NOT EXISTS Users (
+$createUsersTable = "CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,

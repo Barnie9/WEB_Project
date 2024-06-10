ac4c9427c3c4caa41ff015b8fa15fecdc4a2e1fd
Barnie <bran.alexandru.2002@gmail.com>
2024-06-10 08:15:19 +0300

    Added some more info on event card

--- app/src/components/EventCard/EventCard.tsx
+++ app/src/components/EventCard/EventCard.tsx
@@ -1,9 +1,18 @@
 import { useNavigate } from "react-router-dom";
 import { Event } from "../../redux/interfaces/interfaces";
 import EventCardCSS from "./EventCard.module.css";
+import { useGetMatterByIdQuery } from "../../redux/api/matterApi";
+import { useGetUserByIdQuery } from "../../redux/api/userApi";
+import { useGetGroupByIdQuery } from "../../redux/api/groupApi";
+import { useGetRoomByIdQuery } from "../../redux/api/roomApi";
 
 const EventCard = (props: { event: Event }) => {
     const navigate = useNavigate();
+
+    const { data: matter } = useGetMatterByIdQuery(props.event.matter_id);
+    const { data: teacher } = useGetUserByIdQuery(props.event.teacher_id);
+    const { data: group } = useGetGroupByIdQuery(props.event.group_id);
+    const { data: room } = useGetRoomByIdQuery(props.event.room_id);
 
     return (
         <div className={EventCardCSS.card}
@@ -12,10 +21,10 @@ const EventCard = (props: { event: Event }) => {
             }}
         >
             <div className={EventCardCSS.details}>
-                <div>Matter ID: {props.event.matter_id}</div>
-                <div>Teacher ID: {props.event.teacher_id}</div>
-                <div>Group ID: {props.event.group_id}</div>
-                <div>Room ID: {props.event.room_id}</div>
+                <div>Matter: {matter?.message.title}</div>
+                <div>Teacher: {teacher?.message.lastName + " " + teacher?.message.firstName}</div>
+                <div>Group: {group?.message.type + " " + group?.message.number}</div>
+                <div>Room: {room?.message.name}</div>
                 <div>Start Time: {props.event.start_time.toString()}</div>
                 <div>End Time: {props.event.end_time.toString()}</div>
                 <div>Type: {props.event.type}</div>
